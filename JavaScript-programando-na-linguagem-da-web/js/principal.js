68cc50c2ed320248a821354409c9ccea8b72f381
Tiago099 <tiagosebastian99@gmail.com>
2021-10-18 21:47:16 -0300

    colocamos cores para melhor visualização do usuario quando acontecer um erro

--- JavaScript-programando-na-linguagem-da-web/js/principal.js
+++ JavaScript-programando-na-linguagem-da-web/js/principal.js
@@ -22,14 +22,16 @@ var alturaEhValida = true;
 
 if (peso <= 0 || peso >= 1000){ 
     console.log("Peso inválido!");
-    pesoEhValido = false;
     tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+    paciente.classList.add("paciente-invalido");
 }
 
 if (altura <= 0 || altura >= 3.00){
     console.log("Altura inválida!");
-    alturaEhValida = false;
     tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+    paciente.classList.add("paciente-invalido");
 }
 
 if (alturaEhValida && pesoEhValido){
